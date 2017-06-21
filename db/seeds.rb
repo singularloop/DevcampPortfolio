b3efccb7309e1bb31fc1998fafe682d05f6e896e
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 14:27:47 +0000

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "content content content text text text pretend that this all makes sense content content dummy text.")
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized:15
+    )
+  end
+  
+  puts "5 skills created"
+  
+  9.times do |portfolio_item|
+    Portfolio.create!(
+      title: "portfolio title: #{portfolio_item}",
+      subtitle: "My great service",
+      body: "body text",
+      main_image: "https://placeholdit.co//i/600x400?",
+      thumb_image: "https://placeholdit.co//i/300x200?",
+      )
+    end
+  
+  puts "9 portfolio items created"
