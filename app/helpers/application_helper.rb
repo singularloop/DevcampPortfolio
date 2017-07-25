e98c588b965ba96fdb343bd2b427c9af3bf46072
Jordan Hudgens <kalynavery@live.com>
2017-07-25 16:50:29 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-     (link_to "logout", destroy_user_session_path, method: :delete) +
-     "<br>".html_safe +
-     (link_to "edit profile", edit_user_registration_path)
-    else
+    if current_user.is_a?(GuestUser)
      (link_to "register", new_user_registration_path) +
      "<br>".html_safe +
      (link_to "login", new_user_session_path)
+    else
+     (link_to "logout", destroy_user_session_path, method: :delete) +
+     "<br>".html_safe +
+     (link_to "edit profile", edit_user_registration_path)
     end
   end
 
