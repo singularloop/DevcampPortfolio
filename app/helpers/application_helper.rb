5c6ba9e9c9de07165bf1f2200286f051bda09c74
Jordan Hudgens <kalynavery@live.com>
2017-07-27 16:28:42 +0000

    Updated master layout styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-     (link_to "register", new_user_registration_path) +
-     "<br>".html_safe +
-     (link_to "login", new_user_session_path)
+     (link_to "Register", new_user_registration_path, class: style) +
+     " ".html_safe +
+     (link_to "Login", new_user_session_path, class: style)
     else
-     (link_to "logout", destroy_user_session_path, method: :delete) +
-     "<br>".html_safe +
-     (link_to "edit profile", edit_user_registration_path)
+     (link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
+     " ".html_safe +
+     (link_to "Edit Profile", edit_user_registration_path, class: style)
     end
   end
 
